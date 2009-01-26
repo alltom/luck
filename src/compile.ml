14f70fa02cce24311add2e6e93daaea918310b21
Tom Lieber <tom@dynamic-oit-vapornet-b-783.Princeton.EDU>
2009-01-26 13:37:11 -0500

    Fleshed out compiler testing a little more

--- src/compile.ml
+++ src/compile.ml
@@ -1,5 +1,19 @@
 
 open Ast
 
-let compile stmts =
-  []
+exception Compile_error
+
+type typ = IntType
+
+module Context = Map.Make(String)
+
+let rec build_context cntxt decls =
+  match decls with
+    (name, t) :: rest ->
+      build_context (Context.add name IntType cntxt) rest
+    | [] -> cntxt
+
+let compile cntxt stmt =
+  match stmt with
+    ExprStatement (Declaration decls) -> ((build_context cntxt decls), [])
+  | _ -> (cntxt, [])
--- src/compile_test.ml
+++ src/compile_test.ml
@@ -2,5 +2,42 @@
 open Ast
 open Compile
 
+let num_tests = ref 0
+let num_failed = ref 0
+
+let rec check_types cntxt arr =
+  match arr with
+    (name, t) :: rest ->
+      (Context.mem name cntxt)
+        && ((Context.find name cntxt) = t)
+        && (check_types cntxt rest)
+  | [] -> true
+
+let fail () =
+  num_failed := !num_failed + 1;
+  prerr_endline ("FAILED")
+
+(* assert_tree *)
+let t stmts cntxt code =
+  num_tests := !num_tests + 1;
+  try
+    let rescntxt, rescode = compile Context.empty stmts in
+    if (check_types rescntxt cntxt) && (rescode = code) then
+      print_endline ("passed")
+    else
+      fail ()
+  with _ -> fail ()
+
 let _ =
-  print_endline "good job!"
+  let es e = ExprStatement(e) in
+  
+  (* constant expressions with no side effects
+     should compile to nothing *)
+  t NullStatement [] [];
+  t (es (Int 1)) [] [];
+  t (es (Plus(Int 1, Int 2))) [] [];
+  
+  (* declarations should become part of the local context *)
+  t (es (Declaration [("a", Type("int", false, false, []))])) [("a", IntType)] [];
+  
+  print_endline ("failed " ^ (string_of_int !num_failed) ^ " of " ^ (string_of_int !num_tests))

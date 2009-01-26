9e8fdb455b56dbc52d6f87c5bab2bd1c7c437bb4
Tom Lieber <tom@dynamic-oit-vapornet-b-783.Princeton.EDU>
2009-01-26 14:46:39 -0500

    compiler handles declarations a little better

--- src/compile.ml
+++ src/compile.ml
@@ -1,19 +1,35 @@
 
-open Ast
-
 exception Compile_error
 
-type typ = IntType
+type typ =
+  ArrayType of typ
+| RefType of typ
+| IntType
 
 module Context = Map.Make(String)
+
+exception Type_conversion
+
+let rec string_of_type t =
+  match t with
+    ArrayType t' -> (string_of_type t') ^ "[]"
+  | RefType t' -> (string_of_type t') ^ "@"
+  | IntType -> "int"
+
+let rec convert_type asttype =
+  match asttype with
+    Ast.Type("int", false, _, []) -> IntType
+  | Ast.Type(b, false, s, h::r) ->
+      ArrayType(convert_type (Ast.Type(b, false, s, r)))
+  | _ -> raise Type_conversion
 
 let rec build_context cntxt decls =
   match decls with
     (name, t) :: rest ->
-      build_context (Context.add name IntType cntxt) rest
+      build_context (Context.add name (convert_type t) cntxt) rest
     | [] -> cntxt
 
 let compile cntxt stmt =
   match stmt with
-    ExprStatement (Declaration decls) -> ((build_context cntxt decls), [])
+    Ast.ExprStatement (Ast.Declaration decls) -> ((build_context cntxt decls), [])
   | _ -> (cntxt, [])
--- src/compile_test.ml
+++ src/compile_test.ml
@@ -5,28 +5,39 @@ open Compile
 let num_tests = ref 0
 let num_failed = ref 0
 
-let rec check_types cntxt arr =
+let strings_of_test_cntxt cntxt =
+  let f (name, t) = (string_of_type t) ^ " " ^ name in
+  List.map (f) cntxt
+
+let strings_of_real_cntxt cntxt =
+  Context.fold (fun name t lst -> ((string_of_type t) ^ " " ^ name) :: lst) cntxt []
+
+let rec check_vars cntxt arr =
   match arr with
     (name, t) :: rest ->
       (Context.mem name cntxt)
         && ((Context.find name cntxt) = t)
-        && (check_types cntxt rest)
+        && (check_vars cntxt rest)
   | [] -> true
 
-let fail () =
+let fail msg =
   num_failed := !num_failed + 1;
-  prerr_endline ("FAILED")
+  prerr_endline ("FAILED: " ^ msg)
 
-(* assert_tree *)
+(* assertion *)
 let t stmts cntxt code =
   num_tests := !num_tests + 1;
   try
     let rescntxt, rescode = compile Context.empty stmts in
-    if (check_types rescntxt cntxt) && (rescode = code) then
-      print_endline ("passed")
+    if check_vars rescntxt cntxt then
+      (if rescode = code then
+         print_endline ("passed")
+       else fail "code didn't match")
     else
-      fail ()
-  with _ -> fail ()
+      fail ("contexts didn't match: "
+            ^ "expected " ^ (String.concat ", " (strings_of_test_cntxt cntxt)) ^ ", "
+            ^ "got " ^ (String.concat ", " (strings_of_real_cntxt rescntxt)))
+  with _ -> fail "compiler exception"
 
 let _ =
   let es e = ExprStatement(e) in
@@ -39,5 +50,6 @@ let _ =
   
   (* declarations should become part of the local context *)
   t (es (Declaration [("a", Type("int", false, false, []))])) [("a", IntType)] [];
+  t (es (Declaration [("b", Type("int", false, false, [Dynamic]))])) [("b", ArrayType(IntType))] [];
   
   print_endline ("failed " ^ (string_of_int !num_failed) ^ " of " ^ (string_of_int !num_tests))

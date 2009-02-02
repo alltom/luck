02c5f362c8501349a60f7b08202ca4e2a688bb61
Tom Lieber <tom@dynamic-oit-vapornet-d-1370.Princeton.EDU>
2009-02-02 02:27:56 -0500

    loops can be break'd out of

--- src/vm.ml
+++ src/vm.ml
@@ -1,24 +1,31 @@
 
 open Compile
 
-type frame = TopLevel | LoopFrame
+type frame = TopLevel | LoopFrame of bool ref ref * instruction list
 
 let rec run instrs =
   let rec real_run stack =
     match stack with
       [] -> ()
-    | (_, []) :: stack' -> real_run stack'
+    | (t, []) :: stack' ->
+        (match t with
+           TopLevel -> real_run stack'
+         | LoopFrame(cond, body) ->
+             if !(!cond) then
+               real_run ((t, body) :: stack')
+             else
+               real_run stack')
     | (t, (instr :: instrs)) :: stack' ->
         (match instr with
            Op f -> f (); real_run ((t, instrs) :: stack')
-         | Branch (cond, iftrue, iffalse) ->
+         | Branch(cond, iftrue, iffalse) ->
              if !(!cond) then
                real_run ((t, (iftrue @ instrs)) :: stack')
              else
                real_run ((t, (iffalse @ instrs)) :: stack')
-         | Loop (cond, body) ->
+         | Loop(cond, body) ->
              if !(!cond) then
-               real_run ((t, (body @ instrs @ [Loop(cond, body)])) :: stack')
+               real_run ((LoopFrame(cond, body), body) :: (t, instrs) :: stack')
              else
                real_run ((t, instrs) :: stack')
          | Break -> real_run stack')

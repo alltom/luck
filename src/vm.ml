a8a4b7b7d2bcf8fc3e053d49891f30e4c1f5f8c8
Tom Lieber <tom@dynamic-oit-vapornet-d-1370.Princeton.EDU>
2009-02-02 00:53:24 -0500

    revision of vm to make way for break/return support

--- src/vm.ml
+++ src/vm.ml
@@ -1,23 +1,25 @@
 
 open Compile
 
+type frame = Bland | LoopFrame
+
 let rec run instrs =
   let rec real_run stack =
     match stack with
       [] -> ()
-    | [] :: stack' -> real_run stack'
-    | (instr :: instrs) :: stack' ->
+    | (_, []) :: stack' -> real_run stack'
+    | (t, (instr :: instrs)) :: stack' ->
         (match instr with
-           Op f -> f (); real_run (instrs :: stack')
+           Op f -> f (); real_run ((t, instrs) :: stack')
          | Branch (cond, iftrue, iffalse) ->
              if !(!cond) then
-               real_run ((iftrue @ instrs) :: stack')
+               real_run ((t, (iftrue @ instrs)) :: stack')
              else
-               real_run ((iffalse @ instrs) :: stack')
+               real_run ((t, (iffalse @ instrs)) :: stack')
          | Loop (cond, body) ->
              if !(!cond) then
-               real_run ((body @ instrs @ [Loop(cond, body)]) :: stack')
+               real_run ((t, (body @ instrs @ [Loop(cond, body)])) :: stack')
              else
-               real_run (instrs :: stack'))
+               real_run ((t, instrs) :: stack'))
   in
-  real_run [instrs]
+  real_run [(Bland, instrs)]

97b6d311208ad2fe17dded08df334f8333f7da0a
Tom Lieber <tom@dynamic-oit-vapornet-a-286.Princeton.EDU>
2009-04-23 00:10:08 -0400

    take script filenames from command-line, or - for stdin

--- src/luck.ml
+++ src/luck.ml
@@ -4,11 +4,13 @@ open Vm
 
 let arg_print_instrs = ref false
 let window_size = ref 100.0
+let filenames = ref []
 let usage = "usage: " ^ Sys.argv.(0) ^ " [-i] [-w window_size]"
 
 let speclist = [
   ("-i", Arg.Unit (fun () -> arg_print_instrs := true), " print instructions before execution");
-  ("-w <num>", Arg.Int (fun w -> window_size := float_of_int w), " window size");
+  ("-w", Arg.Int (fun w -> window_size := float_of_int w), " window size");
+  ("--", Arg.Rest (fun fname -> filenames := !filenames @ [fname]), " files to load");
 ]
 
 let print_instrs instrs =
@@ -17,13 +19,16 @@ let print_instrs instrs =
     instrs;
   print_endline "---"
 
+let load_file name =
+  let tree = Parser.input Lexer.token (Lexing.from_channel (if name = "-" then stdin else open_in name)) in
+  let shred = Compile.compile Vm.Context.empty tree in (* TODO: get context and stuff from VM for globals *)
+  if !arg_print_instrs then print_instrs (shred_instructions shred);
+  shred
+
 let main () =
-  Arg.parse speclist (fun x -> raise (Arg.Bad ("Bad argument : " ^ x))) usage;
+  Arg.parse speclist (fun fname -> filenames := !filenames @ [fname]) usage;
   try
-    let tree = Parser.input Lexer.token (Lexing.from_channel stdin) in
-    let shred = Compile.compile Vm.Context.empty tree in
-    if !arg_print_instrs then print_instrs (shred_instructions shred);
-    let vm = VM.add VM.empty shred in
+    let vm = List.fold_left (fun vm n -> VM.add vm (load_file n)) VM.empty !filenames in
     let rec run vm =
       let vm = VM.run !window_size vm in
       if VM.running vm then run vm else ()

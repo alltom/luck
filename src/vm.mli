fddb55877402249c15fdc0cca1b184a2535f3625
Tom Lieber <tom@dynamic-oit-vapornet-a-286.Princeton.EDU>
2009-04-22 23:32:03 -0400

    shortened Context signature in vm.mli

--- src/vm.mli
+++ src/vm.mli
@@ -24,24 +24,7 @@ type data =
 | DurData of float
 | TimeData of float
 
-module Context :
-  sig
-    type key = String.t
-    type 'a t = 'a Map.Make(String).t
-    val empty : 'a t
-    val is_empty : 'a t -> bool
-    val add : key -> 'a -> 'a t -> 'a t
-    val find : key -> 'a t -> 'a
-    val remove : key -> 'a t -> 'a t
-    val mem : key -> 'a t -> bool
-    val iter : (key -> 'a -> unit) -> 'a t -> unit
-    val map : ('a -> 'b) -> 'a t -> 'b t
-    val mapi : (key -> 'a -> 'b) -> 'a t -> 'b t
-    val fold : (key -> 'a -> 'b -> 'b) -> 'a t -> 'b -> 'b
-    val compare : ('a -> 'a -> int) -> 'a t -> 'a t -> int
-    val equal : ('a -> 'a -> bool) -> 'a t -> 'a t -> bool
-  end
-
+module Context : Map.S with type key = String.t
 type context = typ Context.t
 
 val builtin_variables : (string * typ) list

c0a6023766acf2b100d42b68cbebc55cd99372d6
Tom Lieber <tom@alltom.com>
2009-05-31 18:52:42 -0400

    simplified compile interface

--- src/compile.mli
+++ src/compile.mli
@@ -3,4 +3,4 @@ exception Compile_error of string (* something is wrong with the input *)
 exception Compiler_error of string (* something went wrong internally *)
 exception Not_implemented of string
 
-val compile : Vm.context -> Ast.func list * Ast.clas list * Ast.stmt list -> Vm.shred_template
+val compile : Vm.context -> Ast.ast -> Vm.shred_template
